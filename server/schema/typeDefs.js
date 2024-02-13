61bec952d942800c4e85246fe5ba63b1ea1ddcba
Pablo Rivera <pablommrivera@gmail.com>
2024-02-12 22:09:30 -0700

    add username to seed data

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -5,6 +5,7 @@ const typeDefs = `
         lastName: String
         email: String
         password: String
+        username: String
     },
     
     type Post {
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -6,3 +6,20 @@ const userData = require('./userData.json');
 const postData = require('./postData.json');
 const commentData = require('./commentData.json');
 
+db.once('open', async () => {
+    try {
+      await cleanDB('User', 'users');
+      await User.create(userData);
+
+      await cleanDB('Post', 'posts');
+      await Post.create(postData);
+
+      await cleanDB('Comment', 'comments');
+      await Comment.create(commentData);
+  
+      console.log('all done!');
+      process.exit(0);
+    } catch (err) {
+      throw err;
+    }
+  });
