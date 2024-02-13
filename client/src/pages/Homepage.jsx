b889aea5660d413baecdf72f66eb476f690815a3
Chasqui10 <chasqui.rmf@gmail.com>
2024-02-12 18:44:36 -0700

    Intial Apoolo testing

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -33,37 +33,35 @@ import Card from '../components/UI/Card/Card';
 // }
 
 
+const Homepage = () => { 
+  const { loading, data } = useQuery(QUERY_POSTS);
 
-
-
-export default function Homepage() {
-  const [crowdfundings, setCrowdfundings] = useState([]);
-  const navigate = useNavigate();
-
-
-  // awaiting database to puill the information from the database
-  useEffect(() => {
-    fetch('/api/crowdfundings')
-      .then((res) => res.json())
-      .then((crowdfundings) => setCrowdfundings(crowdfundings));
-  }, []);
+  const posts = data?.posts || [];
+  
+  console.log(posts);
 
   return (
-    <div className="container pt-4">
-      <h1>Homepage</h1>
+    <div>
+      <h1>Home Page</h1>
       <p>
-        Welcome to the homepage! Here you can find a list of crowdfundings.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
+        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
+        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
+        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
+        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
+        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
+        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
+        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
+        fames ac ante ipsum primis in faucibus.
       </p>
-      <ul>
-        {crowdfundings.map((crowdfunding) => (
-          <li key={crowdfunding.id}>
-            <Link to={`/crowdfundings/${crowdfunding.id}`}>
-              {crowdfunding.title}
-            </Link>
-          </li>
+      <div className="row">
+        {posts.map((post) => (
+          <div className="col-4">
+            <Card title={post.title} description={post.content} />
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
-
