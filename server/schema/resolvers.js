56ed1e8f56d46143ccb2d45f4e4b3d1a944226b6
Pablo Rivera <pablommrivera@gmail.com>
2024-02-13 19:23:18 -0700

    fixed mutation bugs and added mutations for delete

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -13,16 +13,41 @@ const resolvers = {
         }
     },
     Mutation: {
-        addUser: async (parent, {firstName, lastName, email, username}) => {
-            const user = await User.create({firstName, lastName, email, username});
+        addUser: async (_, { firstName, lastName, email, username, password }) => {
+            const user = await User.create({ firstName, lastName, email, username, password });
             return user;
         },
-        addPost: async (parent, {author, title, content }) => {
-            const post = await Post.create({author, title, content});
+        deleteUser: async (_, { id }) => {
+            const user = await User.findByIdAndDelete(id);
+            if (!user) throw new Error("User not found");
+            // Also delete associated posts and comments if necessary
+            return user;
+        },
+
+        addPost: async (_, { title, content, author }) => {
+            const user = await User.findById(author);
+            if (!user) throw new Error("Author not found");
+            const post = await Post.create({ title, content, author: user._id });
             return post;
         },
-        addComment: async (parent, { text, postId, author }) => {
-            const comment = await Comment.create({text, postId, author});
+        deletePost: async (_, { id }) => {
+            const post = await Post.findByIdAndDelete(id);
+            if (!post) throw new Error("Post not found");
+            // Also delete associated comments if necessary
+            return post;
+        },
+
+        addComment: async (_, { text, postId, author }) => {
+            const user = await User.findById(author);
+            if (!user) throw new Error("Author not found");
+            const post = await Post.findById(postId);
+            if (!post) throw new Error("Post not found");
+            const comment = await Comment.create({ text, postId, author: user._id });
+            return comment;
+        },
+        deleteComment: async (_, { id }) => {
+            const comment = await Comment.findByIdAndDelete(id);
+            if (!comment) throw new Error("Comment not found");
             return comment;
         }
     }
